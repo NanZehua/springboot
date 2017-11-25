48419d702684521dcbd465ecbdc2f4ffbf342cf0
nanzehua <842639042@qq.com>
2017-11-25 21:27:57 +0800

    增加日志打印和日志文件路径

--- src/main/java/com/zehua/asj/Application.java
+++ src/main/java/com/zehua/asj/Application.java
@@ -1,12 +1,18 @@
 package com.zehua.asj;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+    protected static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+
+        logger.error("Start");
         SpringApplication.run(Application.class, args);
     }
 }
